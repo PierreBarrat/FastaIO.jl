89433d56bd6527e1a1849d9fa4bd8e82e2c343d1
PierreBarrat <p.barrat@live.fr>
2019-10-25 12:21:06 +0200

    Changes STDOUT to stdout

--- src/FastaIO.jl
+++ src/FastaIO.jl
@@ -346,11 +346,11 @@ mutable struct FastaWriter
     end
 end
 
-FastaWriter() = FastaWriter(STDOUT)
+FastaWriter() = FastaWriter(stdout)
 
 """
     FastaWriter(filename::AbstractString, [mode::String = "w"])
-    FastaWriter([io::IO = STDOUT])
+    FastaWriter([io::IO = stdout])
     FastaWriter(f::Function, args...)
 
 This creates an object which is able to write formatted FASTA files which conform to the specifications
@@ -581,9 +581,9 @@ function writefastaseq(io::IO, seq, entry::Int, nl::Bool = true)
 end
 
 """
-    writefasta([io::IO = STDOUT], data)
+    writefasta([io::IO = stdout], data)
 
-This version of the function writes to an already opened `IO` stream, defaulting to `STDOUT`.
+This version of the function writes to an already opened `IO` stream, defaulting to `stdout`.
 """
 function writefasta(io::IO, data)
     entry = 0
@@ -605,7 +605,7 @@ function writefasta(io::IO, data)
         entry_chars > 0 || error("empty sequence data (entry $entry of FASTA input)")
     end
 end
-writefasta(data) = writefasta(STDOUT, data)
+writefasta(data) = writefasta(stdout, data)
 
 """
     writefasta(filename::String, data, [mode::String = "w"])
